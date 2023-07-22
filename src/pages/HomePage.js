8b303f5ff35798381d900473a7309a24145c34c6
Gayoon1234 <s3843584@student.rmit.edu.au>
2023-07-22 14:23:58 +1000

    Added base layout

---
+++ src/pages/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "../styles/dashboard-grid.css"
+
+function HomePage() {
+  return (
+    <div class="container">
+      <div class="ImageBox Box">
+        <p>Hello</p>
+      </div>
+      <div class="ClockBox Box">
+        <p>Hello</p>
+      </div>
+      <div class="QuoteBox Box">
+        <p>Hello</p>
+      </div>
+      <div class="WeatherBox Box">
+        <p>Hello</p>
+      </div>
+      <div class="TaskBox Box">
+        <p>Hello</p>
+      </div>
+      <div class="MusicBox Box">
+        <p>Hello</p>
+      </div>
+      <div class="TBDBox Box">
+        <p>Hello</p>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
